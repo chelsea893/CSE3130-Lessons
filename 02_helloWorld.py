e829745cdfdb645e3c760fd9b2b92cd5de761f9e
Chelsea Chen <c.chen13@share.epsb.ca>
2021-03-01 14:38:32 -0700

    added program code

--- 02_helloWorld.py
+++ 02_helloWorld.py
@@ -29,13 +29,22 @@ class Window:
         self.SCREEN.fill(self.BACKGROUND)
         self.CAPTION = pygame.display.set_caption(self.TITLE)
 
-
+# --- Modifier Methods --- #
     def updateFrame(self):
         self.FRAME.tick(self.FPS)
         pygame.display.flip()
 
+    def clearFrame(self):
+        self.SCREEN.fill(self.BACKGROUND)
+# --- Accessor Methods --- #
     def getScreen(self):
         return self.SCREEN
+
+    def getVirtualWidth(self):
+        return self.SCREEN.get_rect().width
+
+    def getVirtualHeight(self):
+        return self.SCREEN.get_rect().height
 
 class Text:
     """
@@ -49,6 +58,35 @@ class Text:
         self.X = x
         self.Y = y
         self.POS = (self.X, self.Y)
+        self.DIR_X = 1
+        self.SPEED = 5
+        self.DIR_Y = 1
+    # --- Modifier Methods (Setter) --- #
+    def horizBounce(self, SCREEN):
+        self.X = self.X + self.DIR_X*self.SPEED
+        if self.X > SCREEN.getVirtualWidth() - self.getTextWidth():
+            self.DIR_X = -1
+            self.SPEED = self.SPEED + 0.4
+            self.SCREEN = self.FONT.render(self.TEXT, True, Color.BLUE)
+            self.FONT = pygame.font.SysFont("Arial", 50)
+        if self.X < 0:
+            self.DIR_X = 1
+            self.SPEED = self.SPEED + 0.4
+            self.SCREEN = self.FONT.render(self.TEXT, True, Color.GREEN)
+
+        self.POS = (self.X, self.Y)
+
+    def vertBounce(self, SCREEN):
+        self.Y = self.Y + self.DIR_Y*self.SPEED
+        if self.Y > SCREEN.getVirtualHeight() - self.getTextHeight():
+            self.DIR_Y = -1
+            self.SCREEN = self.FONT.render(self.TEXT, True, Color.RED)
+            self.FONT = pygame.font.SysFont("Arial", 30)
+        if self.Y < 0:
+            self.DIR_Y = 1
+            self.SCREEN = self.FONT.render(self.TEXT, True, Color.WHITE)
+
+        self.POS = (self.X, self.Y)
 
     # --- Accessor Methods (Getter) --- #
 
@@ -58,6 +96,12 @@ class Text:
     def getPOS(self):
         return self.POS
 
+    def getTextWidth(self):
+        return self.SCREEN.get_rect().width
+
+    def getTextHeight(self):
+        return self.SCREEN.get_rect().height
+
 # --- MAIN PROGRAM CODE --- #
 
 if __name__ == "__main__":
@@ -66,13 +110,25 @@ if __name__ == "__main__":
     RUNNING = True
 
     WINDOW = Window()
-    TEXT1 = Text("Hello World")
+    TEXT1 = Text("Hello World", 100, 130)
+    TEXT2 = Text("Hi", 0, 0)
 
     while RUNNING:
+        # Inputs
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 RUNNING = False
                 pygame.quit()
                 sys.exit()
+
+        # Processing
+        TEXT1.horizBounce(WINDOW)
+        TEXT1.vertBounce(WINDOW)
+        TEXT2.horizBounce(WINDOW)
+        TEXT2.vertBounce(WINDOW)
+
+        #OUTPUTS
+        WINDOW.clearFrame()
         WINDOW.getScreen().blit(TEXT1.getText(), TEXT1.getPOS())
+        WINDOW.getScreen().blit(TEXT2.getText(), TEXT2.getPOS())
         WINDOW.updateFrame()
